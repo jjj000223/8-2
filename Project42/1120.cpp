92846291bfe99be930aa0ddaa749e56dc1fd989e
지연 이 <jjj000223@daum.net>
2019-11-20 15:08:06 +0900

    초기 커밋.

---
+++ Project42/1120.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int data1 = 10, data2 = 20;
+	int sum = 0;
+	int* p;
+
+	p = &data1;
+	*p = 100;
+
+	p = &data2;
+	*p = 200;
+
+	sum = data1 + data2;
+	printf("두 변수의 합: %d\n", sum);
+
+}
